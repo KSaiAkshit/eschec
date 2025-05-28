c9c2fc4a5619298dc3027cd5511a5a895a9a90d4
Akira <44054643+KSaiAkshit@users.noreply.github.com>
2025-05-29 01:59:36 +0530

    fix: use correct enemy/ally pieces for move gen

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -4,13 +4,14 @@ use std::str::FromStr;
 use eschec::{Board, Square, fen, moves::MoveGen, perft};
 
 fn main() -> miette::Result<()> {
-    let fen = "rbnqknbr/pppppp1p/8/6p1/8/3P4/PPP1PPPP/RBNQKNBR w KQkq g6 1 2";
+    let fen = "rbnqknbr/1ppppppp/8/p7/PP6/8/2PPPPPP/RBNQKNBR b KQkq b3 1 2";
+
     let b = Board::from_fen(fen);
     println!("{}", b);
 
-    let moves = MoveGen::new(eschec::Piece::Bishop, b.stm, &b);
+    let moves = MoveGen::new(eschec::Piece::Pawn, b.stm, &b);
     for (i, m) in moves.attack_bb.iter().enumerate() {
-        if i == 2 {
+        if i == 32 {
             println!("{}", m.print_bitboard());
         }
     }
